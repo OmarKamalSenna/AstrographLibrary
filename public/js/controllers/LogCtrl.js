2866207bb680b7070e5d70ee78f69f9a20a92071
Mostafa <mostafa@Mostafas-MacBook-Pro.local>
2017-04-05 11:14:56 +0200

    navigation

---
+++ public/js/controllers/LogCtrl.js
@@ -0,0 +1,4 @@
+// public/js/controllers/LogCtrl.js
+angular.module('LogCtrl', []).controller('LogController', function($scope) {
+    $scope.tagline = 'AstrographLibrary Login';
+});
---
+++ public/js/controllers/RegCtrl.js
@@ -0,0 +1,6 @@
+// public/js/controllers/RegCtrl.js
+angular.module('RegCtrl', []).controller('RegController', function($scope) {
+
+    $scope.tagline = 'AstrographLibrary Registeration';
+
+});
