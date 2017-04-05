148802da2c12510cbc2e807204bfc42e96b542f4
OmarKamalSenna <omarmhmdkamal94@gmail.com>
2017-04-05 12:14:27 +0200

    First commit

--- public/js/controllers/MainCtrl.js
+++
@@ -1,6 +0,0 @@
-// public/js/controllers/MainCtrl.js
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
-
-    $scope.tagline = 'AstrographLibrary Home';   
-    $scope.rand = Math.ceil(Math.random()*10);
-});
--- public/js/controllers/RegCtrl.js
+++
@@ -1,6 +0,0 @@
-// public/js/controllers/RegCtrl.js
-angular.module('RegCtrl', []).controller('RegController', function($scope) {
-
-    $scope.tagline = 'AstrographLibrary Registeration';
-
-});
