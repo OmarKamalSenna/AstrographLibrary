fdf2abf195a38b8242b97b88977b7faf61311140
OmarKamalSenna <omarmhmdkamal94@gmail.com>
2017-04-05 12:07:48 +0200

    First commit

--- public/js/controllers/ProfileCtrl.js
+++
@@ -1,6 +0,0 @@
-// public/js/controllers/ProfileCtrl.js
-angular.module('ProfileCtrl', []).controller('ProfileController', function($scope) {
-
-    $scope.tagline = 'AstrographLibrary Profile';
-
-});
