30cd7f05fac6b3665937fe0c0f48db5ec4161805
OmarKamalSenna <omarmhmdkamal94@gmail.com>
2017-04-06 15:33:54 +0200

    First commit

--- models/user.js
+++ models/user.js
@@ -1,49 +1,78 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
-var multer = require('multer');
-var upload = multer({dest : 'public/uploads'})
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
 
-/*mongoose.connect("mongodb://localhost/auth");
-var db = mongoose.connection;*/
-var UserSchema = mongoose.Schema({
-	username: {
-		type: String,
-		index:true
-	},
-	password: {
-		type: String
-	},
-	email: {
-		type: String
-	},
-	name: {
-		type: String
-	}
+//El schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    usertype: {
+        type: Boolean,
+        required: true
+    }
 });
 
-var User = module.exports = mongoose.model('User', UserSchema);
+const User = module.exports = mongoose.model('User', UserSchema);
 
-module.exports.createUser = function(newUser, callback){
-	bcrypt.genSalt(10, function(err, salt) {
-	    bcrypt.hash(newUser.password, salt, function(err, hash) {
-	        newUser.password = hash;
-	        newUser.save(callback);
-	    });
-	});
+module.exports.getUserById = function (id, callback) {
+    User.findById(id, callback);
 }
 
-module.exports.getUserByUsername = function(username, callback){
-	var query = {username: username};
-	User.findOne(query, callback);
+module.exports.getUserByUsername = function (username, callback) {
+    const query = { username: username };
+    User.findOne(query, callback);
 }
 
-module.exports.getUserById = function(id, callback){
-	User.findById(id, callback);
+module.exports.getRandomBusinesses = function (callback) {
+    //  const query = { usertype: true };
+    //    User.aggregate([{$sample: {size: 2}}], function(err, data){
+    //        if(err) throw err;
+
+    //        return data;
+    //    });
+    //   User.find();
+    //   User.findOne({_id: '58e507d9b504da233884f514'}, callback);
+    //     User.find().lean().exec(function (err, data) {
+    //         // so now, we can return all students to the screen.
+    //         if(err) throw err;
+    //          JSON.stringify(data);
+    //     });
+    // }
+
+    User.aggregate([{ $sample: { size: 4 } }], function(err, res) { 
+       if(err) throw err;
+      // console.log(res);
+       callback(err,res);
+     }); 
 }
 
-module.exports.comparePassword = function(candidatePassword, hash, callback){
-	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-    	if(err) throw err;
-    	callback(null, isMatch);
-	});
+module.exports.addUser = function (newUser, callback) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
+
+module.exports.comparePassword = function (candidatePassword, hashedPassword, callback) {
+    bcrypt.compare(candidatePassword, hashedPassword, (err, isMatch) => {
+        if (err) throw err;
+        callback(null, isMatch);
+    });
 }
