662c3ac25287e6bdf96a74277272a5d6915bee7b
Ehab42 <ihabnoureldin9@gmail.com>
2017-04-22 15:51:28 +0200

    sp2

---
+++ src/routes/api.js
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import authenticationController from './controllers/authentication';
+import businessesController from './controllers/businesses';
+import businessRegistrationsController from './controllers/businessRegistrations';
+import activitiesController from './controllers/activities';
+import activityBookingsController from './controllers/activityBookings';
+import activityTypesController from './controllers/activityTypes';
+import paymentMethodController from './controllers/paymentMethod';
+
+export default ({ db }) => {
+  const api = Router();
+
+  /** Signup & Login */
+  authenticationController({ api, db });
+
+  /** Businesses */
+  businessesController({ api, db });
+
+  /** Business Registrations */
+  businessRegistrationsController({ api, db });
+
+  /** Activities */
+  activitiesController({ api, db });
+
+  /** Activity Bookings & Activity Booking Requests */
+  activityBookingsController({ api, db });
+
+  /** Activity Types & Activity Type Addition Requests */
+  activityTypesController({ api, db });
+
+  /**payment*/
+  paymentMethodController({api,db});
+
+  return api;
+};
